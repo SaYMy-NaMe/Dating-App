ac5347183200fd428f38d400dfa5e320f1d52bdf
ashrafur <ashrafurrahman42069@gmail.com>
2024-12-18 12:32:39 +0600

    Running Section 4: Auth Basics: 36. Adding a login Endpoint

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad3f5ec879c8b3ef370c35e27d370025fb89ab0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b13ba5e1413d008ad838190be8f560269dd425")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
