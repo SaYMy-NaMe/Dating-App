d35ccff049cf3105c8039d28bed31b41c3ba49cc
ashrafur <ashrafurrahman42069@gmail.com>
2024-12-24 16:35:33 +0600

    Running Section 5: Client Login and Register: 47. Angular Services

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14685f1a5c059f74674220ccf5907cdf45c89b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302ca7b7871d604ae68ce93444651eaf5b08d04a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
