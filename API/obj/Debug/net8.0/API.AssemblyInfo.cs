7064714bbef497248dc331da85a24f34f51a6d63
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-01 16:10:42 +0600

    64. Added Angular Auth Guard in routes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e796d564f56dcbd2a2f9920f00d7f1a658b772f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfb5b311e5512783f6c61c76ce3d255a972aaf97")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
