3941c0c422f8cdd56f352c1a7c1f90ee0b10aa6e
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-01 12:17:16 +0600

    55. Parent to Child Component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5bd73b0962128d656d6500c75de1be815721f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c9b0edddf024b4ec1a194e2dbb9b498c44fa87")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
