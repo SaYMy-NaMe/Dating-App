a5bffd5d065f6bd819894580a4c48788b7c94807
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-06 19:03:21 +0600

    Section 7: Error Handling: 73. Validation Error Handled

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49cc0e884538af742bfab610cb8db4cf050bddf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9394bddb4f9d7457b6247c05a992566619f8f697")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
