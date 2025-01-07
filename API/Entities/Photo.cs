8d14b3c9cd01651df233bbf71b82ccedd2567f57
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-07 16:35:23 +0600

    Section 8. Extending the API: Extending the User Entity and Commenting Register Controller for now

---
+++ API/Entities/Photo.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public required string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string? PublicId { get; set; }
+}
