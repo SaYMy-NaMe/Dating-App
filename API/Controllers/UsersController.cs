1fe598b26777c4f9c717b806de9a03e60c8db029
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-08 19:14:01 +0600

    Section 8: Extending the API: 87. Updating the users controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,27 +1,27 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await context.Users.ToListAsync(); 
-        return users; 
+        var users = await userRepository.GetUsersAsync();
+        return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id:int}")] // api/users/id
-    public async Task<ActionResult<AppUser>> GetUser(int id)
+    [HttpGet("{username}")] // api/users/lisa
+    public async Task<ActionResult<AppUser>> GetUser(string username)
     {
-        var user = await context.Users.FindAsync(id); 
-        if(user == null) return NotFound(); 
-        return user; 
+        var user = await userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return NotFound();
+        return user;
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c64b2a210f4d4cff68e6f4286cc16d671996da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e54f13c9fe6f0eec0094cd4063c0f325687556")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
