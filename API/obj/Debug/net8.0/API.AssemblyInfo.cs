50cc1bdcdcc284c8e0777f9d56e2b6a5ae567b98
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-09 12:55:07 +0600

    Section 9. Building the UI: 94-96: Intro, TypScript Basics, Creating the member Interface

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec26ca43df9c42629a82a664ec609b93770a72ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1240ffc83ef16a56453aa3e339b340aab0b590")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
