b6ed6c477fddc0e6565225a6c44a4fd83ddff18b
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-09 13:24:13 +0600

    Section 9: Building the UI: 97. Adding a Member Service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d1240ffc83ef16a56453aa3e339b340aab0b590")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50cc1bdcdcc284c8e0777f9d56e2b6a5ae567b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
