35a2006e93941445da3ca2764e04ad0d9cdd16bc
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-12 15:18:40 +0600

    End of Section 9: Building the UI: 106. Adding a Photo Gallery and a Dummy image if user doesn't any image

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6581e8151cc688a4ab748155ed2a586b80678f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a72f1e8c0f383e87f17d4b95adcef58288fa78cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
