b50e4713a70456a84c4a4d57222a7feb6d785e5d
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-12 18:40:50 +0600

    Section 10. Updating Resources: 110. Creating the edit template form

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a2006e93941445da3ca2764e04ad0d9cdd16bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438d290fb4c4ebe2f8c2848bb638687b095daae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
