a72f1e8c0f383e87f17d4b95adcef58288fa78cf
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-12 14:43:14 +0600

    Save changes before Angular update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24eea7ed57a9a86fed5280f0f9d4b502976d7768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6581e8151cc688a4ab748155ed2a586b80678f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
