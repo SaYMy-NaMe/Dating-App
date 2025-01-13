707312bc1befceb5c8623506554d02be64b01cfb
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-13 13:20:33 +0600

    Section 10. Updating the Resources: 112. Adding a Can Deactivate route guard

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b50e4713a70456a84c4a4d57222a7feb6d785e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c689d987d694e7de6e3cb960337686b748779d54")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
