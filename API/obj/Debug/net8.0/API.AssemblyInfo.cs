948788411261607f26bf02be68e404a585207a7e
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-13 17:50:35 +0600

    Section 10. Updating Resources: 115. Adding loading indicators

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bdd0763e03889d49b3bbd33da0d8092e93a762d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8dd9c6826e712c7926e311ffc17e0dc0221354b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
