4e57f69effd45bfcf96197b0460eb6046873ca69
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-15 15:02:38 +0600

    Section 11. Adding Photo upload functionality: 125. Adding a Photo Editor Component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2844d54bd02421992a671cafe8092b44f8e46930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f486201ae09675d7a62be133ed877d82e5edc69")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
