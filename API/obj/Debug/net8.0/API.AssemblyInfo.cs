110957dd6f040a0fa2a9322ff86d6149d0f61392
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-16 17:31:49 +0600

    Section 11. Adding Photo upload functionality: 131. Setting the main photo in the client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472703c9420cacc595361053383094bfee4deddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6623e6f5bcc1c7043d787eff1767f58ff9929a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
