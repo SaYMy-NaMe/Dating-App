53a3fd9c5e526ade1aebae737864f658c8a52896
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-16 13:26:27 +0600

    Section 11. Adding Photo upload functionality: 128. Adding a photo uploader part two

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a19f0ac7f9dac6287975b4b87bb3a545e6d297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78475f1ba15e4c6ae955b6a3c1497322a8ddc772")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, input, output } from '@angular/core';
 import { Member } from '../../_models/member';
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
@@ -8,7 +8,7 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-photo-editor',
   standalone: true,
-  imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule],
+  imports: [NgIf, NgFor, NgStyle, NgClass, FileUploadModule, DecimalPipe],
   templateUrl: './photo-editor.component.html',
   styleUrl: './photo-editor.component.css',
 })
@@ -16,7 +16,7 @@ export class PhotoEditorComponent {
   private accountService = inject(AccountService);
   member = input.required<Member>();
   uploader?: FileUploader;
-  hasBaseDropzoneOver = false;
+  hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   memberChange = output<Member>();
 
@@ -24,7 +24,7 @@ export class PhotoEditorComponent {
     this.initializeUploader();
   }
   fileOverBase(e: any) {
-    this.hasBaseDropzoneOver = e;
+    this.hasBaseDropZoneOver = e;
   }
   initializeUploader() {
     this.uploader = new FileUploader({
