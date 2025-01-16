c6623e6f5bcc1c7043d787eff1767f58ff9929a7
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-16 14:53:03 +0600

    Section 11. Adding Photo upload functionality: 130. Adding the main photo image to the nav bar

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a3fd9c5e526ade1aebae737864f658c8a52896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472703c9420cacc595361053383094bfee4deddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
