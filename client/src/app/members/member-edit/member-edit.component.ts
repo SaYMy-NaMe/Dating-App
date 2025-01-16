78475f1ba15e4c6ae955b6a3c1497322a8ddc772
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-16 13:02:56 +0600

    Section 11. Adding Photo upload functionality: 127. Adding a photo uploader

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -11,7 +11,7 @@ import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { PhotoEditorComponent } from "../photo-editor/photo-editor.component";
+import { PhotoEditorComponent } from '../photo-editor/photo-editor.component';
 
 @Component({
   selector: 'app-member-edit',
@@ -48,5 +48,8 @@ export class MemberEditComponent implements OnInit {
         this.editForm?.reset(this.member);
       },
     });
+  }
+  onMemberChange(event: Member) {
+    this.member = event;
   }
 }
