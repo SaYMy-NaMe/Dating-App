fc34bff1f75f6b8d67c49c2b3071ec9d175ad81c
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-19 16:04:26 +0600

    Section 12: Reactive Forms: 144. Updating the API register method

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16372f7d585eb5c51cf3473adb1c926cc539ae8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a153853cebfd18397b08a9509546f2947828a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
