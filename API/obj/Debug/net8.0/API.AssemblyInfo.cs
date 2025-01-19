ddda04b21dbf7e586b2c37b9081f70736d6990c4
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-19 19:11:57 +0600

    Section 13: Paging, Sorting and Filtering: 151. Setting up client pagination

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0150bac1cae1c72ed0692a6817863736193d41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc24f0b15e314b58022e34a1102a80bd934f5393")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
