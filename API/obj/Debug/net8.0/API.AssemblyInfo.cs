a0cc75d9adb2c545e7e2bad7783152505d4722de
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-20 12:37:36 +0600

    Section 13: Paging, Sorting and Filtering: 158. Adding sorting functionality in API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a1ad7db661dff9de2eee2e55609c2527b4e627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b566ab1e968219f5ed77c1781d500481818afb28")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
