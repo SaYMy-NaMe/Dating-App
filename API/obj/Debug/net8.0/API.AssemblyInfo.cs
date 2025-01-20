59c0c7a21965d2510cb752944887529225bc3486
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-20 17:18:45 +0600

    End of Section 13: Paging, Sorting and Filtering: 164. Remembering the filters for a user in the service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad6967583ffe85460170d7ac818e869779597ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ce8926467bff40d1d504163a454ec98a632c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
