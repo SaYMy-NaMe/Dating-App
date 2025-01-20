02ce8926467bff40d1d504163a454ec98a632c2c
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-20 16:56:06 +0600

    Section 13: Paging, Sorting and Filtering: 163. Restoring caching for member details

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525b1b23f353e72af229c0ea503e8f853ee606bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad6967583ffe85460170d7ac818e869779597ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
