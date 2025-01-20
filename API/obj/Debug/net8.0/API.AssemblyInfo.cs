b566ab1e968219f5ed77c1781d500481818afb28
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-20 12:27:39 +0600

    Section 13: Paging, Sorting and Filtering: 156. Adding filter buttons to the client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85dcfe711c74ca1634dca1bf951b8c24f0701df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88a1ad7db661dff9de2eee2e55609c2527b4e627")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
