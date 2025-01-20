995c5f32a5625cd2c65caca90e7c0d90279e33f4
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-20 12:51:42 +0600

    Section 13: Paging, Sorting and Filtering: 158. Sorting on the client\]

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b566ab1e968219f5ed77c1781d500481818afb28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cc75d9adb2c545e7e2bad7783152505d4722de")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
