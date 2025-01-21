7eb7323267ff819b958dda6f1ec9913f80698ea4
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-21 15:54:54 +0600

    Section 14: Adding the likes feature: 171. Setting up the likes functions in the Angular App

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd08420b8a5dd5f85e9655587f4834b235c63563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18eb44c58930664dfa4fad74344255e985741ad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
