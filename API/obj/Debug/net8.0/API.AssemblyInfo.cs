18eb44c58930664dfa4fad74344255e985741ad3
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-21 15:39:11 +0600

    Section 14: Adding the likes feature: 170. Controller methods for the likes feature

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa21b43ad24d3d247da5bb319f4884711e8b282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd08420b8a5dd5f85e9655587f4834b235c63563")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
