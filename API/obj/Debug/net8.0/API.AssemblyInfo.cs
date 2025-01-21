5554b51470536e27867e08a24e642d82da051416
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-21 19:17:41 +0600

    Section 15. Adding the Messaging Feature: 181. Adding a message controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6595dd18f55cf7bbcaa662c39bfc5298e0ff29b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310abd9b18f550aef040fc2a21bfe822a11d0811")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
