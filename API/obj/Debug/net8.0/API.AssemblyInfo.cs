7a0df3dc03673a039bc4ce6dac046bd58f1b80ba
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-22 18:40:19 +0600

    Section 15. Adding the Messaging Feature: 193. Deleting messages on the API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8cc037a33d4727a16f95a85bcac93e0a00600c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c9a8cc498f990131ad8e0fe253e7660a832e63")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
