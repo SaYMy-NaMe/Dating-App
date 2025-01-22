65c9a8cc498f990131ad8e0fe253e7660a832e63
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-22 16:54:12 +0600

    Section 15. Adding the Messaging Feature: 192. Fixing the photo weirdness

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -25,6 +25,9 @@ export class MessagesComponent implements OnInit {
   container = 'Inbox';
   pageNumber = 1;
   pageSize = 5;
+
+  isOutbox = this.container === 'Outbox';
+
   ngOnInit(): void {
     this.loadMessages();
   }
