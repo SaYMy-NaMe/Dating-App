c4fe775d9e8a3b33ef9ce708542d7d628465d41e
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-22 15:34:16 +0600

    Section 15. Adding the Messaging Feature: 189. Using query params

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3d6e3453a430625a733deb3d9fd7b052bf3828")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4989d1672d0307260ad0a19482cc7a4438a52ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
