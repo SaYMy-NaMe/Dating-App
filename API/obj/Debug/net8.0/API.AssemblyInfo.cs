9d7c3925db428fcaf8e5d1d21e8a726a2be4becf
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-23 12:56:52 +0600

    Section 16. Identity and role management: 201. Updating the seed method and update database

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7640c6ba36dd72f7e8f53b66daeca7fd35a7846d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bbbfef50da79e2be51b04ea04d4127de8ed2af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
