98fb58722e2f5addf2e7ed98bde92a02ce62a949
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-23 12:08:04 +0600

    Section 16. Identity and role management: 199. Configuring the DbContext

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a59e3e3f080cee0611b86df45bd7de81aa63fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d31b4f4402c1fafcf1bec07152b438a8997c29")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
