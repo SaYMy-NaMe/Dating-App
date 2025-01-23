c5db836de8faec994d626eb9636aaecf09638c83
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-23 13:49:20 +0600

    Section 16. Identity and role management: 203. Adding roles to the app

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7c3925db428fcaf8e5d1d21e8a726a2be4becf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccfe7a9ffedd08fa72ef967d6c709e1fe71c4c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
