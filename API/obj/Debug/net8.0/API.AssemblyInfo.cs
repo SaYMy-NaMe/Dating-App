e8f5ab26eb245a1375c0e65de89aef4bfe6f22f5
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-26 13:12:18 +0600

    Section 16. Identity and role management: 213. Editing roles in the client part one

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1387e105e51ea3b31586238c2df51762c3845cfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a85a0511f82a59ddfa81fc6c5a17eeab10e36d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
