de3ae316c2dc6b266d0949d9e80898074b705a97
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-26 17:06:02 +0600

    Section 17: SignalR: 221. Displaying online presence (220. Adding a Presence tracker)

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60893d79bda4fbc5b6d120e4b3a257782628d2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce36a72b99637fe75b77f08e68864d7e76682750")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
