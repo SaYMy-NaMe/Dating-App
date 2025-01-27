9fb5207ddfc13d9f0773986a5555b8a29d231f9a
ashrafur <ashrafurrahman42069@gmail.com>
2025-01-27 14:54:21 +0600

    Section 17: SignalR: 231. Subscribing to route parameter changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca40855f917184caf3d14d2eef771c0bb3cf1aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949a59dcc73cfea914db592ac59260b6394aeaa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
